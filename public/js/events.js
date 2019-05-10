6e02b804822e1c34d4238613447097b04a176feb
pbrayer <skellocrusha@yahoo.com>
2019-05-10 17:54:20 -0500

    removed handlebars, added unfavorite functionality

--- public/js/events.js
+++ public/js/events.js
@@ -1,9 +1,9 @@
 $(function() {
   // Function for creating a new list row for Events
   $(document).on("click", ".foundationSuxx", function(){
-  var id = $(this).data("id");
+  let id = $(this).data("id");
 console.log(id)
-  var newFavoriteState = {
+  let newFavoriteState = {
     favorited: 1
   };
 
@@ -20,20 +20,41 @@ console.log(id)
   );
 });
 
+$(document).on("click", ".unFav", function(){
+    let id = $(this).data("id");
+  console.log(id)
+    let newFavoriteState = {
+      favorited: 0
+    };
+  
+    // Send the PUT request.
+    $.ajax("/api/events/" + id, {
+      type: "PUT",
+      data: newFavoriteState
+    }).then(
+      function() {
+        console.log("changed event status to unfavorited");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
   $.get("/api/events", function(data) {
     for(var i = 0; i < data.length; i++) {
     let x = i+1
     let newTr = $("<tr>");
     newTr.append("<td>" + data[i].title + "</td>");
     newTr.append("<td>" + data[i].date + "</td>")
+    $(".foundationSuxx").addClass("success button")
+    $(".unFav").addClass("alert button")
 
    if(data[i].favorited == 0){
     newTr.append("<td>" + "<button type = " + "button " + "data-id = " + x + " class = " + "foundationSuxx" + ">" + "Favorite" + "</button>" + "</td>")
-    $(".foundationSuxx").addClass("hollow button success")
     $("#events").append(newTr)
    }
     else{
-    newTr.append("<td>" + "<button type = " + "button " + "data-id = " + x + " class = " + "foundationSuxx" + ">" + "Unfavorite" + "</button>" + "</td>")
+    newTr.append("<td>" + "<button type = " + "button " + "data-id = " + x + " class = " + "unFav" + ">" + "Unfavorite" + "</button>" + "</td>")
     $("#favorited-events").append(newTr)
     }
 }
